4a47636812a9f14b105fd602c911e7144a4a205d
Leandro Motta Junior <lemottajr@hotmail.com>
2025-06-15 21:16:07 -0300

    Making some code changes

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,52 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["⚽️", "🥅", "🏟️", "🏆", "😘", "❤️", "🎶", "🥶", "🧠", "🧤"]
-    @State var emojiCount = 6
+    var emojis = ["⚽️", "🥅", "🏟️", "🏆", "😘", "❤️", "🎶", "🥶", "🧠", "🧤", "🥳", "🤝", "🎩"]
+    @State var cardCount = 6
     
     var body: some View {
-        VStack{
-            HStack {
-                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                        CardView(content: emoji)
-                    }
+        VStack {
+            cards
+            Spacer()
+            cardCountAdjusters
+        }
+        .padding()
+        
+    }
+    
+    var cards: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))]) {
+                ForEach(emojis[0..<cardCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                        .aspectRatio(2/3, contentMode: .fit)
                 }
             }
-            .foregroundColor(.gray)
-            Spacer()
-            HStack {
-                removeCardButton
-                Spacer()
-                addCardButton
-                
-            }
-            .font(.largeTitle)
-            .padding(.horizontal)
         }
-        .padding(.horizontal)
-        
+        .foregroundColor(.gray)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            removeCardButton
+            Spacer()
+            addCardButton
+            
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
         
     }
     
     var removeCardButton: some View {
         Button {
-            if emojiCount > 1 {
-                emojiCount -= 1
+            if cardCount > 1 {
+                cardCount -= 1
             }
         } label: {
-            Image(systemName: "minus.rectangle.portrait")
+            Image(systemName: "rectangle.stack.badge.minus.fill")
         }
+        .disabled(cardCount < 2)
     }
     
     var addCardButton: some View {
         Button {
-            if emojiCount < emojis.count {
-                emojiCount += 1
+            if cardCount < emojis.count {
+                cardCount += 1
             }
         } label: {
-            Image(systemName: "plus.rectangle.portrait")
+            Image(systemName: "rectangle.stack.badge.plus.fill")
             }
+        .disabled(cardCount == emojis.count)
         }
     
 }
@@ -67,7 +78,7 @@ struct CardView: View {
             let shape = RoundedRectangle(cornerRadius: 20)
             if isFaceUp{
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content).font(.largeTitle)
             } else {
                 shape.fill()
