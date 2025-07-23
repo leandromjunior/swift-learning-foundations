741db33656778d6f06a3ba5ac99289765f54ff7c
Leandro Motta Junior <lemottajr@hotmail.com>
2025-07-23 00:51:32 -0300

    Completing day 16

---
+++ 100DaysSwiftUI/Day 16 - WeSplit Theory/WeSplit/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Leandro Motta Junior on 22/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var tapCount = 0 // The @state term "replaces" the mutating in some swiftUi cases
+    @State private var name = ""
+    
+    let students = ["Harry", "Hermione", "Ron", "None"]
+    @State private var selectedStudent = "None"
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Enter your name", text: $name) // The dollar sign means that we are reading and writing the variable 'var name' "at the same time"
+                    Text("Hello, \(name)")
+                }
+                
+                Section {
+                    Text("Hello World")
+                    Text("Hello World")
+                    Text("Hello World")
+                }
+                
+                Section {
+                    ForEach(0..<3) { number in // This is a closure
+                        Text("Row \(number)")
+                    }
+                }
+                
+                Section {
+                    Picker("Select your student", selection: $selectedStudent) {
+                        // 'self' identify every view on the screen uniquely
+                        ForEach(students, id: \.self) {student in // This is a closure
+                            Text(student)
+                        }
+                    }
+                }
+                
+                Text("Hello World")
+                Text("Hello World")
+                
+                Section {
+                    Button("Tap Count: \(tapCount)") {
+                        self.tapCount += 1
+                    }
+                }
+            }
+            .navigationTitle("SwiftUi")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DaysSwiftUI/Day 16 - WeSplit Theory/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Leandro Motta Junior on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
