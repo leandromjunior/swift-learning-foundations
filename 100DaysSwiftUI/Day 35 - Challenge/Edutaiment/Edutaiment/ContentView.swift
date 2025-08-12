b753cc8acc3f9afaf712c2fc79864e179db29fac
Leandro Motta Junior <lemottajr@hotmail.com>
2025-08-12 00:05:42 -0300

    Completing the core of the day 35 challenge

--- 100DaysSwiftUI/Day 35 - Challenge/Edutaiment/Edutaiment/ContentView.swift
+++ 100DaysSwiftUI/Day 35 - Challenge/Edutaiment/Edutaiment/ContentView.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let numberOfQuestions = [5, 10, 15]
+    
     @State private var table = 2
     @State private var questionPicker = 5
     @State private var randomNum = Int.random(in: 1...40)
     @State private var answer = 0
-    @State private var showAlert = false
     @State private var score = 0
-    @State private var scoreTitle = ""
-    @State private var scoreMessage = ""
     @State private var count = 0
     
     @State private var animationValue = 1.0
     
+    @State private var showAlert = false
+    @State private var showEndAlert = false
     
-    let numberOfQuestions = [5, 10, 15]
+    @State private var scoreTitle = ""
+    @State private var endTitle = ""
+    
     var body: some View {
         ZStack {
             List {
@@ -43,10 +47,22 @@ struct ContentView: View {
                     .keyboardType(.decimalPad)
             }
             
+            VStack(spacing: 20) {
+                
+                Text("Play: \(count)")
+                Text("Score: \(score)")
+            
+            }
+            .font(.title.bold())
+            
             VStack {
+                Spacer()
+                Spacer()
+                
                 Button("Continue") {
                     
                     checkAnswer(table, randomNum)
+                    continueGame()
                 }
                 .frame(width: 100, height: 50)
                 .background(Color(red: 127/255, green: 0, blue: 255/255))
@@ -56,7 +72,15 @@ struct ContentView: View {
                 .alert(scoreTitle, isPresented: $showAlert) {
                     Button("OK", action: continueGame)
                 }
+                .alert(endTitle, isPresented: $showEndAlert) {
+                    Button("Reset", action: resetGame)
+                } message: {
+                    Text("Your final score is \(score)")
+                }
+                
+                Spacer()
             }
+            
         }
     }
     
@@ -65,16 +89,29 @@ struct ContentView: View {
         let realAnswer = number1 * number2
         
         if realAnswer == answer {
-            scoreTitle = "Well Done!"
             score += 10
         } else {
-            scoreTitle = "Oops! That's not right"
+            scoreTitle = "Oops! That's not correct"
+            showAlert.toggle()
         }
         
-        showAlert.toggle()
+        count += 1
+        
+        if count == questionPicker {
+            endTitle = "Game Over"
+            showEndAlert.toggle()
+        }
     }
     
     func continueGame() {
+        randomNum = Int.random(in: 1...40)
+        answer = 0
+    }
+    
+    func resetGame() {
+        
+        count = 0
+        score = 0
         randomNum = Int.random(in: 1...40)
         answer = 0
     }
