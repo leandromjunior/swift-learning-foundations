3be727bff0352c09aa8f8cfb5a7c2a8fe487f35b
Leandro Motta Junior <lemottajr@hotmail.com>
2025-08-13 01:17:53 -0300

    Completing day 37

---
+++ 100DaysSwiftUI/Day 36-38 - iExpense-Project 7/iExpense/iExpense/AddView.swift
@@ -0,0 +1,49 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Leandro Motta Junior on 12/08/25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Business", "Personal"]
+    
+    var expenses: Expenses
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Expense Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new Expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
