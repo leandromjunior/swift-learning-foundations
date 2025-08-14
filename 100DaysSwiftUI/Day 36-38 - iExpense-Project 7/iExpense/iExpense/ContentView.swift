e6dd95f708eecbe27d1ba9bc8e6448b51a3860ed
Leandro Motta Junior <lemottajr@hotmail.com>
2025-08-14 02:04:44 -0300

    Completing partially the day 38 challenge

--- 100DaysSwiftUI/Day 36-38 - iExpense-Project 7/iExpense/iExpense/ContentView.swift
+++ 100DaysSwiftUI/Day 36-38 - iExpense-Project 7/iExpense/iExpense/ContentView.swift
@@ -220,6 +220,65 @@ struct ContentView: View {
 
 //Day 38 - Make some notes of what each line or each excerpt do in the "ContentView" above
 
+/*
+ 1 - Use the user’s preferred currency, rather than always using US dollars.
+ 2 - Modify the expense amounts in ContentView to contain some styling depending on their value – expenses under $10 should have one style, expenses under $100 another, and expenses over $100 a third style. What those styles are depend on you.
+ 3 - For a bigger challenge, try splitting the expenses list into two sections: one for personal expenses, and one for business expenses. This is tricky for a few reasons, not least because it means being careful about how items are deleted!
+ */
+
+struct ChallengeContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text(item.name)
+                                .font(.headline)
+                            
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        if item.amount == 0 {
+                            Text(item.amount, format: .currency(code: "BRL"))
+                                .foregroundStyle(.green)
+                        } else if item.amount < 10 {
+                            Text(item.amount, format: .currency(code: "BRL"))
+                                .foregroundStyle(.yellow)
+                        } else if item.amount < 100 {
+                            Text(item.amount, format: .currency(code: "BRL"))
+                                .foregroundStyle(.orange)
+                        } else {
+                            Text(item.amount, format: .currency(code: "BRL"))
+                                .foregroundStyle(.red)
+                        }
+                            
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
 #Preview {
-    ContentView()
+    ChallengeContentView()
 }
