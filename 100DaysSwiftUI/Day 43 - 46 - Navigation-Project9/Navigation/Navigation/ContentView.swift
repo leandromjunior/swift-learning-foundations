a13ad82c0a00bafba9aa53112b6073a3dd980294
Leandro Motta Junior <lemottajr@hotmail.com>
2025-08-19 12:56:32 -0300

    Completing day 44

--- 100DaysSwiftUI/Day 43 - 46 - Navigation-Project9/Navigation/Navigation/ContentView.swift
+++ 100DaysSwiftUI/Day 43 - 46 - Navigation-Project9/Navigation/Navigation/ContentView.swift
@@ -41,6 +41,257 @@ struct NavigationDestinationView2: View {
     }
 }
 
+// Day 44
+
+//Programmatic navigation with NavigationStack
+struct ProgrammaticNavigationView: View {
+    @State private var path = [Int]()
+    var body: some View {
+        NavigationStack(path: $path) {
+            VStack {
+                Button("Show 32") {
+                    path = [32]
+                }
+                
+                Button("Show 64") {
+                    path.append(64)
+                }
+                
+                Button("Show 32 then 64") {
+                    path = [32, 64]
+                }
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
+            }
+        }
+    }
+}
+
+// Navigatin to different data types using NavigationPath
+struct NavigationPathView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(0..<5) { i in
+                    NavigationLink("Select Number: \(i)", value: i)
+                }
+                
+                ForEach(0..<5) { i in
+                    NavigationLink("Select String: \(i)", value: String(i))
+                }
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected the number \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the string \(selection)")
+            }
+        }
+    }
+}
+
+struct NavigationPathView2: View {
+    // Different from the code of "ProgrammaticNavigationView", now we might have integers OR strings so we can't use a simple array any more
+    @State private var path = NavigationPath()
+    var body: some View {
+        NavigationStack(path: $path) {
+            List {
+                ForEach(0..<5) { i in
+                    NavigationLink("Select Number: \(i)", value: i)
+                }
+                
+                ForEach(0..<5) { i in
+                    NavigationLink("Select String: \(i)", value: String(i))
+                }
+            }
+            .toolbar {
+                Button("Push 556") {
+                    path.append(556)
+                }
+                
+                Button("Push Hello") {
+                    path.append("Hello")
+                }
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected the number \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the string \(selection)")
+            }
+        }
+    }
+}
+
+//How to make a NavigationStack return to its root view programmatically
+struct DetailView: View {
+    var number: Int
+    var body: some View {
+        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+    }
+}
+
+struct NormalProgrammaticView: View {
+    @State private var path = [Int]()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            DetailView(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i)
+                }
+        }
+    }
+}
+
+struct DetailView2: View {
+    var number: Int
+    @Binding var DetailViewPath: [Int]
+    
+    var body: some View {
+        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    DetailViewPath.removeAll()
+                }
+            }
+    }
+}
+
+struct RootProgrammaticView: View {
+    @State private var path = [Int]()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            DetailView2(number: 0, DetailViewPath: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView2(number: i, DetailViewPath: $path)
+                }
+        }
+    }
+}
+
+// Now, the exact same code above but in case of we want to use the NavigationPath()
+struct DetailView3: View {
+    var number: Int
+    @Binding var DetailViewPath: NavigationPath
+    
+    var body: some View {
+        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    DetailViewPath = NavigationPath()
+                }
+            }
+    }
+}
+
+struct RootProgrammaticNavigationPathView: View {
+    @State private var path = NavigationPath()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            DetailView3(number: 0, DetailViewPath: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView3(number: i, DetailViewPath: $path)
+                }
+        }
+    }
+}
+
+// How to save NavigationStack paths using codable
+@Observable
+class PathStore {
+    var path: [Int] {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "savedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+                path = decoded
+                return
+            }
+        }
+        
+        path = []
+    }
+    
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
+// The same code above but in caso of we want to use NavigationPath
+@Observable
+class PathStoreNavigationPath {
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "savedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
+struct DetailView4: View {
+    var number: Int
+    
+    var body: some View {
+        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+    }
+}
+
+// Now when we close the app and reopen it, we are in the same page we were at
+struct SavingNavigationPathView: View {
+    @State private var pathStore = PathStoreNavigationPath()
+    
+    var body: some View {
+        NavigationStack(path: $pathStore.path) {
+            DetailView4(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView4(number: i)
+                }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -54,5 +305,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    NavigationDestinationView2()
+    SavingNavigationPathView()
 }
