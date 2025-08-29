fba8f1c06cc7d12342a164adcd22b26bfbeeb0b7
Leandro Motta Junior <lemottajr@hotmail.com>
2025-08-28 22:58:39 -0300

    Completing day 51

--- 100DaysSwiftUI/Day 49-52 - CupcakeCorner-Project10/CupcakeCorner/CupcakeCorner/Order.swift
+++ 100DaysSwiftUI/Day 49-52 - CupcakeCorner-Project10/CupcakeCorner/CupcakeCorner/Order.swift
@@ -8,7 +8,19 @@
 import Foundation
 
 @Observable
-class Order {
+class Order: Codable {
+    enum Codingkeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
     var type = 0
@@ -23,7 +35,34 @@ class Order {
             }
         }
     }
+    
     var extraFrosting = false
     var addSprinkles = false
     
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    var cost: Decimal {
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+        
+        // Complicated cakes cost more
+        cost += Decimal(type) / 2
+        
+        //$1 per cake for sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+        
+        return cost
+    }
 }
