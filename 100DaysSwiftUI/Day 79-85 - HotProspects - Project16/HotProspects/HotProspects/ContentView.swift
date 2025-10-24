d2a661438edeac00cf50c333312a7efc01e77762
Leandro Motta Junior <lemottajr@hotmail.com>
2025-10-24 00:59:22 -0300

    Completing day 80

--- 100DaysSwiftUI/Day 79-85 - HotProspects - Project16/HotProspects/HotProspects/ContentView.swift
+++ 100DaysSwiftUI/Day 79-85 - HotProspects - Project16/HotProspects/HotProspects/ContentView.swift
@@ -55,6 +55,93 @@ struct Tabs: View {
     }
 }
 
+// Swift's Result type
+struct ResultType: View {
+    @State private var output = ""
+    var body: some View {
+        Text(output)
+            .task {
+                await fetchReadingsTask()
+            }
+    }
+    
+    func fetchReadings() async {
+        do {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            output = "Found \(readings.count) readings"
+        } catch {
+            print("Download error")
+        }
+    }
+    
+    func fetchReadingsTask() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+        // We can do this
+        do {
+            output = try result.get()
+        } catch {
+            output = "Error: \(error.localizedDescription)"
+        }
+        
+        // OR we can do this
+        switch result {
+        case .success(let str):
+            output = str
+        case .failure(let error):
+            output = "Error: \(error.localizedDescription)"
+        }
+    }
+}
+
+// Controlling image interpolation in SwiftUI
+struct ImageInterpolation: View {
+    var body: some View {
+        Image(.example)
+            .interpolation(.none) // Adding this line, the image is not blur anymore
+            .resizable()
+            .scaledToFit()
+            .background(.black)
+    }
+}
+
+// Creating Context Menus
+struct ContextMenus: View {
+    @State private var backgroundColor = Color.red
+    var body: some View {
+        VStack {
+            Text("Hello World!")
+                .padding()
+                .background(backgroundColor)
+            Text("Change Color")
+                .padding()
+                .contextMenu { // Context Menu -> It shows a menu by pressing a component
+                    //The role .destructive make the foregroundColor red
+                    Button("Red", systemImage: "checkmark.circle.fill", role: .destructive) {
+                        backgroundColor = .red
+                    }
+                    
+                    Button("Green") {
+                        backgroundColor = .green
+                    }
+                    
+                    Button("Blue") {
+                        backgroundColor = .blue
+                    }
+                }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -69,6 +156,9 @@ struct ContentView: View {
 
 #Preview {
     //SelectItemsInList()
-    Tabs()
+    //Tabs()
+    //ResultType()
+    //ImageInterpolation()
+    ContextMenus()
     //ContentView()
 }
