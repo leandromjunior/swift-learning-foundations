7ed09d39bea4137ddd38a1c97ebd6f45b6e02720
Leandro Motta Junior <lemottajr@hotmail.com>
2025-10-30 23:50:49 -0300

    Completing day 84

--- 100DaysSwiftUI/Day 79-85 - HotProspects - Project16/HotProspects/HotProspects/MeView.swift
+++ 100DaysSwiftUI/Day 79-85 - HotProspects - Project16/HotProspects/HotProspects/MeView.swift
@@ -15,6 +15,8 @@ struct MeView: View {
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     
+    @State private var qrCode = UIImage()
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -26,13 +28,23 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your Code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in
+                updateCode()
+            }
+            .onChange(of: emailAddress) { _ in
+                updateCode()
+            }
         }
     }
     
@@ -40,12 +52,16 @@ struct MeView: View {
         filter.message = Data(string.utf8)
         
         if let outputImage = filter.outputImage {
-            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgImage)
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
             }
         }
         
         return UIImage(systemName: "xmark.cicle") ?? UIImage()
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
 }
 
