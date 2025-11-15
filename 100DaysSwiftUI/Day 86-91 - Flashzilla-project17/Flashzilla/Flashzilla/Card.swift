ddac045bba54faf9fae1c71cd9798ed3555f5d51
Leandro Motta Junior <lemottajr@hotmail.com>
2025-11-14 22:03:22 -0300

    Completing day 88

---
+++ 100DaysSwiftUI/Day 86-91 - Flashzilla-project17/Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Leandro Motta Junior on 14/11/25.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "What's your name?", answer: "Leandro")
+}
