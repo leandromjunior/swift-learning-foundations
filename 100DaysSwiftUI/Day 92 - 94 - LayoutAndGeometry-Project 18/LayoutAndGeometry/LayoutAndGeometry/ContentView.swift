1e174de929c481f8c7dcf4f18aaf6cef76754876
Leandro Motta Junior <lemottajr@hotmail.com>
2025-11-24 08:30:54 -0300

    Completing day 94

--- 100DaysSwiftUI/Day 92 - 94 - LayoutAndGeometry-Project 18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 100DaysSwiftUI/Day 92 - 94 - LayoutAndGeometry-Project 18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -204,6 +204,43 @@ struct ScrollViewEffect: View {
     }
 }
 
+// Challenges
+struct Challenge: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    let fadeOutDistance: CGFloat = 200
+    let minScale: CGFloat = 0.5
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        let offsetY = proxy.frame(in: .named("Scroll")).minY
+                        let normalizedOffset = max(0, min(1, 1 - (offsetY - fadeOutDistance) / fullView.size.height))
+                        let opacity = Double(normalizedOffset)
+                        let scale = max(minScale, 1 - normalizedOffset * (1 - minScale))
+                        
+                        let hue = min(Double(normalizedOffset) * 0.6, 1.0)
+                        let saturation: Double = 0.8
+                        let brightness: Double = 0.8
+                        let color = Color(hue: hue, saturation: saturation, brightness: brightness)
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            //.background(colors[index % 7])
+                            .background(color) // challenge 3
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(opacity) // challenge 1
+                            .scaleEffect(scale) // challenge 2
+                    }
+                    .frame(height: 40)
+                }
+            }
+            .coordinateSpace(name: "Scroll")
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -224,6 +261,7 @@ struct ContentView: View {
     //AbsolutePositioning()
     //ResizingImageWithGeometryReader()
     //FramesAndCoordinates()
-    ScrollViewEffect()
+    //ScrollViewEffect()
+    Challenge()
     //ContentView()
 }
